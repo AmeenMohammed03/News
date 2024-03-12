123dbd88a7eac7c8430fe8c31d8b950dc817c456
Mohammed Ameen <127188975+AmeenMohammed03@users.noreply.github.com>
2024-03-12 11:53:12 +0530

    Modified Article class.

--- app/src/main/java/com/example/news/models/Article.kt
+++ app/src/main/java/com/example/news/models/Article.kt
@@ -1,15 +1,8 @@
 package com.example.news.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-import com.example.news.models.Source
 import java.io.Serializable
 
-@Entity(
-    tableName = "articles"
-)
 data class Article(
-    @PrimaryKey(autoGenerate = true)
     var id: Int? = null,
     val author: String,
     val content: String,
